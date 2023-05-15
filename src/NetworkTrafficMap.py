9240338e7ea007533aa3726a22faee321aefad9b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-05-15 18:18:58 -0500

    add rates

---
+++ src/NetworkTrafficMap.py
@@ -0,0 +1,39 @@
+from util import Util
+from HashMap import HashMap
+class NetworkTrafficMap(object):
+    def __init__(self):
+        self.all = HashMap()
+        self.current = HashMap()
+        
+
+    @property
+    def requests_no_all(self):
+        return self.all.size
+    @property
+    def requests_no_current(self):
+        return self.current.size
+    def add(self, addr,conn):
+        ip = Util.ipkey(addr)
+
+        data = {
+            'key':ip,
+            'ip':addr[0],
+            'port':addr[1],
+            'requests':0,
+            'addr':addr,
+            'conn':conn,
+            'date':Util.timestamp(),
+
+        }
+        self.all.add(ip, data)
+        self.current.add(ip, data)
+    def remove(self,addr):
+        ip = Util.ipkey(addr)
+        self.current.remove(ip)
+    def update_all_request_no(self,addr):
+        ip = Util.ipkey(addr)
+        self.all.get(ip)['requests'] += 1
+    def update_current_request_no(self,addr):
+        ip = Util.ipkey(addr)
+        self.current.get(ip)['requests'] += 1
+    
